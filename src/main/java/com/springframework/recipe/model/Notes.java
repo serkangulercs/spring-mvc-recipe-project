76b0491d92c7dfc3b828986678d283b4c3d17f08
serkan <serkanguler.cs@gmail.com>
2021-05-24 14:39:53 +0300

    Creating notes model. Closes #4.

---
+++ src/main/java/com/springframework/recipe/model/Notes.java
@@ -0,0 +1,23 @@
+package com.springframework.recipe.model;
+
+public class Notes {
+
+    private Recipe recipe;
+    private String recipeNotes;
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
--- src/main/java/com/springframework/recipe/model/Recipe.java
+++ src/main/java/com/springframework/recipe/model/Recipe.java
@@ -11,4 +11,69 @@ public class Recipe {
     //todo add
     //private Difficulty difficulty;
     private Byte[] image;
+    private Notes notes;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
 }
