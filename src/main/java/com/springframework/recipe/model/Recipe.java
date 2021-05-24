f13422aa793685bc08fbc6f4cd6c739f22a83b2b
serkan <serkanguler.cs@gmail.com>
2021-05-24 14:37:55 +0300

    Creating recipe model. Closes #3.

---
+++ src/main/java/com/springframework/recipe/model/Recipe.java
@@ -0,0 +1,14 @@
+package com.springframework.recipe.model;
+
+public class Recipe {
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String direction;
+    //todo add
+    //private Difficulty difficulty;
+    private Byte[] image;
+}
