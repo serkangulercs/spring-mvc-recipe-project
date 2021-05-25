f08206fc2b14289cf228cb01e134f7ee1c7a7b47
serkan <serkanguler.cs@gmail.com>
2021-05-25 14:55:49 +0300

    Adding spring data JPA Query methods.

--- src/main/java/com/springframework/recipe/controller/IndexController.java
+++ src/main/java/com/springframework/recipe/controller/IndexController.java
@@ -1,13 +1,34 @@
 package com.springframework.recipe.controller;
 
+import com.springframework.recipe.model.Category;
+import com.springframework.recipe.model.UnitOfMeasure;
+import com.springframework.recipe.repository.CategoryRepository;
+import com.springframework.recipe.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is:" + categoryOptional.get().getId());
+        System.out.println("UOM Id is:" + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/com/springframework/recipe/repository/UnitOfMeasureRepository.java
+++ src/main/java/com/springframework/recipe/repository/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.springframework.recipe.repository;
 import com.springframework.recipe.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
