885f845e4f5263177fb21d720a29fe83f1a96bfe
serkan <serkanguler.cs@gmail.com>
2021-05-29 17:33:38 +0300

    Adding lombok plugin and refactoring.

--- src/main/java/com/springframework/recipe/controller/IndexController.java
+++ src/main/java/com/springframework/recipe/controller/IndexController.java
@@ -1,16 +1,12 @@
 package com.springframework.recipe.controller;
 
-import com.springframework.recipe.model.Category;
-import com.springframework.recipe.model.UnitOfMeasure;
-import com.springframework.recipe.repository.CategoryRepository;
-import com.springframework.recipe.repository.UnitOfMeasureRepository;
 import com.springframework.recipe.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
-
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -22,6 +18,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("Getting Index Page");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
