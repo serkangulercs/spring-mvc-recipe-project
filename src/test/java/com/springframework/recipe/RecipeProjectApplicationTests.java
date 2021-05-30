77b494b0954ebac81e7cfafde9cf2dcb29b365ef
serkan <serkanguler.cs@gmail.com>
2021-05-30 20:03:07 +0300

    Introduction to JUnit

--- src/test/java/com/springframework/recipe/RecipeProjectApplicationTests.java
+++ src/test/java/com/springframework/recipe/RecipeProjectApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RecipeProjectApplicationTests {
 
-
-
+    @Test
+    public void contextLoads(){
+    }
 }
---
+++ src/test/java/com/springframework/recipe/model/CategoryTest.java
@@ -0,0 +1,32 @@
+package com.springframework.recipe.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setup() {
+        category = new Category();
+    }
+
+    @Test
+    public void getId() {
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
