6faee4a36b787d9f4ac7c37ff20e3a2747155ff9
serkan <serkanguler.cs@gmail.com>
2021-05-30 20:14:48 +0300

    Using Mockito Mocks

--- src/test/java/com/springframework/recipe/model/CategoryTest.java
+++ src/test/java/com/springframework/recipe/model/CategoryTest.java
@@ -2,7 +2,6 @@ package com.springframework.recipe.model;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.Assert.assertEquals;
 
---
+++ src/test/java/com/springframework/recipe/service/RecipeServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.springframework.recipe.service;
+
+import com.springframework.recipe.model.Recipe;
+import com.springframework.recipe.repository.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet recipeData = new HashSet();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
